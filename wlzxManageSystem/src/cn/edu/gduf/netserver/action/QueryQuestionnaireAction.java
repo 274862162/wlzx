577430f60200bb9fc686af5174e49b37bbee5825
wmj <978926785@qq.com>
2016-02-27 22:02:41 +0800

    修复JDBC驱动包版本异常

--- wlzxManageSystem/src/cn/edu/gduf/netserver/action/QueryQuestionnaireAction.java
+++ wlzxManageSystem/src/cn/edu/gduf/netserver/action/QueryQuestionnaireAction.java
@@ -16,25 +16,23 @@ public class QueryQuestionnaireAction implements Action{
 		//得到问卷列表
 		ISurveySystemQueryQusnDao queryImpl=new SurveySystemQueryQusnDaoImpl();
 		//需从session获取用户ID
-		int userID=(Integer)request.getSession().getAttribute("id");
-		//往session保存当前页
-		request.getSession().setAttribute("currentPage", 1);
-		//往session中存放结果集条数
-		request.getSession().setAttribute("allPage", queryImpl.allPage(userID));
-		
-		ArrayList<QusnnList> qusnnLists=queryImpl.queryQusnn(userID,1);
-		//把数据从servlet传到视图页面
-		request.setAttribute("qusnnLists", qusnnLists);
-		//控制台输出数据
-		/*
-		for(int i=0;i<qusnnLists.size();i++){
-			System.out.println(qusnnLists.get(i).getQusnnID()+" "+qusnnLists.get(i).getQuesnnTitle()
-					+" "+qusnnLists.get(i).getQusnnStatus()+" "+qusnnLists.get(i).getQuesnnTime()
-					+" "+qusnnLists.get(i).getReceiveData());
+		int userID=0;
+		if(request.getSession().getAttribute("id")!=null){
+			userID=(Integer)request.getSession().getAttribute("id");
 		}
-		*/
-		//返回转发页面
-		return "../jsp/surveySystemIndex.jsp";
+		if(userID!=0){
+			//往session保存当前页
+			request.getSession().setAttribute("currentPage", 1);
+			//往session中存放结果集条数
+			request.getSession().setAttribute("allPage", queryImpl.allPage(userID));
+			
+			ArrayList<QusnnList> qusnnLists=queryImpl.queryQusnn(userID,1);
+			//把数据从servlet传到视图页面
+			request.setAttribute("qusnnLists", qusnnLists);
+			return "../jsp/surveySystemIndex.jsp";
+		}else{
+			return "../jsp/login.jsp";
+		}
 	}
 
 }
